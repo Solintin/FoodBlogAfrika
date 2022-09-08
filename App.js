e2ca096ce83b8f30eaa474ea4aea85708ab8d4af
DevSaalih <soliualley@gmail.com>
2022-09-08 15:47:47 +0100

    Update

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 //Imports ---declarations
 const express = require("express");
-require("dotenv").config();
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+const xss = require("xss-clean");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 require("express-async-errors"); //Replaces user defined try catch errors in controllers.
+require("dotenv").config();
 const connectDb = require("./db/dbConfig");
 const port = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGO_URI
@@ -15,7 +20,21 @@ const authRoute = require("./routes/authRoute")
 const app = express();
 // Middlewares
 app.use(express.json());
+const morgan = require("morgan");
 
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 60,
+    max:60
+  })
+);
+app.use(xss());
+app.use(mongoSanitize());
+app.use(helmet());
 
 
 
