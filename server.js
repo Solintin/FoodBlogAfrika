17cf0d4fde68b66d1e8bb8ad36ee2435c5abc73c
DevSaalih <soliualley@gmail.com>
2022-09-09 09:12:32 +0100

    Update

--- server.js
+++ server.js
@@ -44,16 +44,16 @@ app.use("/api/v1/auth", authRoute);
 app.use(notFound)
 app.use(errorHandler)
 
-const start = async () => {
-  try {
-    await connectDb(MONGODB_URI);
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+// const start = async () => {
+//   try {
+//     await connectDb(MONGODB_URI);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-start();
+// start();
 
