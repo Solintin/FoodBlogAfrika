6e67707c032a30850106fcf637c3ddff6e638b51
DevSaalih <soliualley@gmail.com>
2022-09-20 14:52:08 +0100

    Update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -9,7 +9,6 @@ const {
   resetPassword,
   resetNewPassword
 } = require("../controllers/authController");
-const requestTest = require("../controllers/testrequest");
 
 router.post("/register", Register);
 router.post("/login", Login);
@@ -17,6 +16,5 @@ router.post("/verify", Verify);
 router.post("/forgetpassword", forgetPassword);
 router.post("/resetpassword", resetPassword);
 router.post("/resetNewPassword", resetNewPassword);
-router.post("/requestTest", requestTest);
 
 module.exports = router;
